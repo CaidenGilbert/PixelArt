af4c4abf0b31056e3cd94890fbf1ea048906a1c1
jotr5402 <jotr5402@colorado.edu>
2025-04-02 17:29:14 -0600

    fix typos and make single use variables const

--- ProjectSourceCode/src/public/color_picker.js
+++ ProjectSourceCode/src/public/color_picker.js
@@ -1,7 +1,7 @@
 const preview_canvas = document.getElementById("color_preview");
-let preview_context = preview_canvas.getContext("2d");
+const preview_context = preview_canvas.getContext("2d");
 const hue_slider_canvas = document.getElementById("hue_slider");
-let hue_slider_context = hue_slider_canvas.getContext("2d");
+const hue_slider_context = hue_slider_canvas.getContext("2d");
 
 drawInitialCanvases();
 addClickEvent();
@@ -10,10 +10,10 @@ function drawInitialCanvases() {
   preview_context.fillStyle = "black";
   preview_context.fillRect(0, 0, preview_canvas.width, preview_canvas.height);
 
-  let gradient = hue_slider_context.createLinearGradient(0, 0, hue_slider_canvas.width, 0);
+  const gradient = hue_slider_context.createLinearGradient(0, 0, hue_slider_canvas.width, 0);
 
   for (let i = 0; i <= 360; i++) {
-      let [r, g, b] = hsvToRGB(i, 1, 1);
+      const [r, g, b] = hsvToRGB(i, 1, 1);
       gradient.addColorStop(i / 360, `rgba(${r}, ${g}, ${b}, 1)`);
   }
 
@@ -30,8 +30,8 @@ function addClickEvent() {
     // convert x pos to hue (in degrees)
     const hue = (pos / this.width) * 360;
 
-    let [r, g, b] = hsvToRGB(hue, 1, 1);
-    preview_context.fillStyle = `rgba(${r}, ${g}, ${b})`;
+    const [r, g, b] = hsvToRGB(hue, 1, 1);
+    preview_context.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
     preview_context.fillRect(0, 0, preview_canvas.width, preview_canvas.height);
   });
 }
