d1afbcf3793572a014684b4cc2d15a84fd9c6a09
jotr5402 <jotr5402@colorado.edu>
2025-04-03 18:14:25 -0600

    add saturation and value spectrum to further change color

--- ProjectSourceCode/src/public/color_picker.js
+++ ProjectSourceCode/src/public/color_picker.js
@@ -1,38 +1,96 @@
 const preview_canvas = document.getElementById("color_preview");
 const preview_context = preview_canvas.getContext("2d");
+
 const hue_slider_canvas = document.getElementById("hue_slider");
 const hue_slider_context = hue_slider_canvas.getContext("2d");
 
+const sv_slider_canvas = document.getElementById("sv_slider");
+const sv_slider_context = sv_slider_canvas.getContext("2d");
+
+const color = {h: 0, s: 0, v: 0, r: 0, g: 0, b: 0};
+
 drawInitialCanvases();
-addClickEvent();
+addClickEvents();
 
 function drawInitialCanvases() {
   preview_context.fillStyle = "black";
   preview_context.fillRect(0, 0, preview_canvas.width, preview_canvas.height);
 
-  const gradient = hue_slider_context.createLinearGradient(0, 0, hue_slider_canvas.width, 0);
+  const hue_gradient = hue_slider_context.createLinearGradient(0, 0, hue_slider_canvas.width, 0);
 
   for (let i = 0; i <= 360; i++) {
       const [r, g, b] = hsvToRGB(i, 1, 1);
-      gradient.addColorStop(i / 360, `rgba(${r}, ${g}, ${b}, 1)`);
+      hue_gradient.addColorStop(i / 360, `rgba(${r}, ${g}, ${b}, 1)`);
   }
 
-  hue_slider_context.fillStyle = gradient;
+  hue_slider_context.fillStyle = hue_gradient;
   hue_slider_context.fillRect(0, 0, hue_slider_canvas.width, hue_slider_canvas.height);
+
+  // no saturation gradient because default is black
+  const value_gradient = hue_slider_context.createLinearGradient(0, 0, 0, sv_slider_canvas.height);
+  for (let i = 0; i <= 100; i++) {
+    // split 255 numbers into 100 possible values
+    // subtract by 255 because max value is at min y
+    const [r, g, b] = [
+      255 - (2.55 * i),      
+      255 - (2.55 * i),      
+      255 - (2.55 * i),      
+    ];
+    value_gradient.addColorStop(i / 100, `rgba(${r}, ${g}, ${b}, 1)`);
+  }
+
+  sv_slider_context.fillStyle = value_gradient;
+  sv_slider_context.fillRect(0, 0, sv_slider_canvas.width, sv_slider_canvas.height);
 }
 
-function addClickEvent() {
+function addClickEvents() {
+  sv_slider_canvas.addEventListener('click', function(event) {
+    // get mouse position relative to canvas
+    const rect = this.getBoundingClientRect();
+    const pos = {
+      x: (event.clientX - rect.left) / (rect.right - rect.left) * this.width,
+      y: (event.clientY - rect.bottom) / (rect.top - rect.bottom) * this.height
+    }
+
+    // console.log(pos);
+
+    // convert pos to saturation and value (0-1)
+    // x pos is saturation, y is value
+    color.s = pos.x / this.width;
+    color.v = pos.y / this.height;
+    [color.r, color.g, color.b] = hsvToRGB(color.h, color.s, color.v);
+
+    preview_context.fillStyle = `rgba(${color.r}, ${color.g}, ${color.b}, 1)`;
+    preview_context.fillRect(0, 0, preview_canvas.width, preview_canvas.height);
+  }); 
+
   hue_slider_canvas.addEventListener('click', function(event) {
     // get mouse position relative to canvas
     const rect = this.getBoundingClientRect();
     const pos = (event.clientX - rect.left) / (rect.right - rect.left) * this.width;
 
     // convert x pos to hue (in degrees)
-    const hue = (pos / this.width) * 360;
+    color.h = (pos / this.width) * 360;
+    [color.r, color.g, color.b] = hsvToRGB(color.h, 1, 1);
 
-    const [r, g, b] = hsvToRGB(hue, 1, 1);
-    preview_context.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
-    preview_context.fillRect(0, 0, preview_canvas.width, preview_canvas.height);
+    // display saturation/value gradient
+    // start with saturation
+    const saturation_gradient = sv_slider_context.createLinearGradient(0, 0, sv_slider_canvas.width, 0);
+    saturation_gradient.addColorStop(0, "#FFFFFF");
+    saturation_gradient.addColorStop(1, `rgba(${color.r}, ${color.g}, ${color.b}, 1)`);
+    
+    sv_slider_context.fillStyle = saturation_gradient;
+    sv_slider_context.fillRect(0, 0, sv_slider_canvas.width, sv_slider_canvas.height);
+
+    // then add value
+    const value_gradient = sv_slider_context.createLinearGradient(0, 0, 0, sv_slider_canvas.height);
+    value_gradient.addColorStop(0, "#FFFFFF");
+    value_gradient.addColorStop(1, "#000000");
+
+    sv_slider_context.globalCompositeOperation = 'multiply';
+    sv_slider_context.fillStyle = value_gradient;
+    sv_slider_context.fillRect(0, 0, sv_slider_canvas.width, sv_slider_canvas.height);
+    sv_slider_context.globalCompositeOperation = 'source-over';
   });
 }
 
@@ -83,5 +141,3 @@ function hsvToRGB(hue, saturation, value) {
 
   return [255 * (r + m), 255 * (g + m), 255 * (b + m)]; 
 }
-
-
