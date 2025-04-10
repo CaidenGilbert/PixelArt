81d1eb04588783131da6842bf44636351ce3c222
jotr5402 <jotr5402@colorado.edu>
2025-04-09 18:30:21 -0600

    remove redundant code

--- ProjectSourceCode/src/public/js/color_picker.js
+++ ProjectSourceCode/src/public/js/color_picker.js
@@ -1,4 +1,4 @@
-import { drawInitialCanvases, addClickEvents, hsvToRGB } from "./color_utils.js";
+import { drawInitialCanvases, addClickEvents } from "./color_utils.js";
 const preview_canvas = document.getElementById("color_preview");
 const hue_slider_canvas = document.getElementById("hue_slider");
 const sv_slider_canvas = document.getElementById("sv_slider");
