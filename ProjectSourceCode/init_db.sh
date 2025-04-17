e3a7b86193a7d96901dadf10630841fc6ae00445
Conrad Sadler <141091490+conradSadler@users.noreply.github.com>
2025-04-17 14:51:32 -0600

    Delete ProjectSourceCode/init_db.sh

--- ProjectSourceCode/init_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# DO NOT PUSH THIS FILE TO GITHUB
-# This file contains sensitive information and should be kept private
-PG_URI=process.env.POSTGRES_External_Database_URL
-# Execute each .sql file in the directory
-for file in init_data/*.sql; do
-    echo "Executing $file..."
-    psql $PG_URI -f "$file"
-done
