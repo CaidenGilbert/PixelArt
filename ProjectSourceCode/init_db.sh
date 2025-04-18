f4b2dc217ce27b9f214e8440657853e4159e4436
Conrad Sadler <141091490+conradSadler@users.noreply.github.com>
2025-04-18 08:04:02 -0600

    Delete ProjectSourceCode/init_db.sh

--- ProjectSourceCode/init_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# DO NOT PUSH THIS FILE TO GITHUB
-# This file contains sensitive information and should be kept private
-PG_URI='postgresql://users_db_pwim_user:6MSS8GiMz360nQ2MWcbGPbKnA6n7yyar@dpg-cvvvafl6ubrc73akjuig-a.virginia-postgres.render.com/users_db_pwim'
-# Execute each .sql file in the directory
-for file in src/init_data/*.sql; do
-    echo "Executing $file..."
-    psql $PG_URI -f "$file"
-done
